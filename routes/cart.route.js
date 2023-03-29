50e6d6c15e23d8bcf967ef0b2a873a1aeb2715ad
shridhar <shrihakke14@gmail.com>
2023-03-29 17:20:13 +0530

    backend Neutrinos Completed

--- routes/cart.route.js
+++ routes/cart.route.js
@@ -8,7 +8,6 @@ const cartRoute = Router();
 //
 // cartRoute.use(authmiddleware);
 
-
 cartRoute.get("/", async (req, res) => {
   const carts = await cartModel.find().populate("productId");
   return res.send(carts);
@@ -19,7 +18,7 @@ cartRoute.post("/", async (req, res) => {
       productId: req.body.productId,
     });
     if (!product) {
-      const cart = await cartModel.create({ ...req.body});
+      const cart = await cartModel.create({ ...req.body });
       return res.send({ message: "added to cart", data: cart });
     } else {
       return res.status(403).send({ message: "already added to the cart" });
@@ -32,6 +31,13 @@ cartRoute.delete("/", async (req, res) => {
   const carts = await cartModel.deleteMany({});
   return res.send({
     message: "Chekout Successfull",
+  });
+});
+cartRoute.delete("/:id", async (req, res) => {
+  let { id } = req.params;
+  const carts = await cartModel.findOneAndDelete({ _id: id });
+  return res.status(403).send({
+    message: "cart removed",
   });
 });
 
